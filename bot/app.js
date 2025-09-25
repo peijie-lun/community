2263440deca2864b790f28cdbf86ee865993df9e
aliciahsieh <alicia40526@gmail.com>
2025-09-26 00:44:20 +0800

    chore: add start script & listen on PORT

--- bot/app.js
+++ bot/app.js
@@ -1,53 +1,36 @@
 require('dotenv').config();
-
 const express = require('express');
 const line = require('@line/bot-sdk');
-require('dotenv').config();
 
-const line = require('@line/bot-sdk');
-const express = require('express');
-
-// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
-// create LINE SDK client
 const client = new line.Client(config);
-
-// create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
+
+app.get('/', (_, res) => res.status(200).send('OK'));
+
+// LINE Webhook
+app.post('/callback', line.middleware(config), async (req, res) => {
+  try {
+    const results = await Promise.all(req.body.events.map(handleEvent));
+    res.json(results);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 });
 
-// event handler
-function handleEvent(event) {
+async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
+    return null; // 非文字訊息就忽略
   }
-
-  // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
 
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
+const PORT = process.env.PORT || 3000;   
+app.listen(PORT, () => console.log('l
